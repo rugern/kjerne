9c658da2e1f8aa7e3040248a1930770f7c01e894
jhnoor <jamawadi@gmail.com>
2013-03-04 14:35:34 +0100

    Preliminary changes to EventMaker
    also test of commit

--- src/datamodell/EventMaker.java
+++ src/datamodell/EventMaker.java
@@ -9,12 +9,36 @@ public class EventMaker {
 		this.email = email;
 	}
 	
-	public void log_in(String username, String password) {
+	/**
+	 * Log in method. The username and password is checked, throws exception if error.
+	 * @param username
+	 * @param password
+	 * @throws WrongPasswordException if password wrong, UserNameNotFoundException 
+	 * if username not found.
+	 */
+	public void log_in(String username, String password) throws Exception {
 		
+		//Exception if wrong password
+		//throw new Exception("Incorrect password");
+		
+		//Exception if username not found
+		//throw new Exception("Username not found");
 	}
 	
+	/**
+	 * User wants to create an event, we create the event object.
+	 * The data for this object is gotten via GUI.
+	 * @return the created Event object
+	 */
 	public Event createEvent() {
 		
+		Event event = new Event();
+		
+		//TODO
+		//1. get data from GUI
+		//2. set data into event
+		
+		return event; //TODO currently returns an empty event, see above!
 	}
 	
 	public void saveEvent() {
@@ -25,8 +49,10 @@ public class EventMaker {
 		
 	}
 	
+
 	public Event getEvent() {
 		
+		return null; //TODO
 	}
 	
 	public boolean[][] getCalendar() {
