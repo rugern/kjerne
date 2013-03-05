5d1daa4d1067012aa63cc6e1cc6aa958a300d5aa
rugern <nirugsveen@live.no>
2013-03-05 11:18:03 +0100

    Changed event

--- src/datamodell/Event.java
+++ src/datamodell/Event.java
@@ -1,9 +1,8 @@
 package datamodell;
 
 import java.sql.Time;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.Locale;
+import java.util.List;
 
 public class Event {
 	
@@ -13,12 +12,11 @@ public class Event {
 	private Locale locale;
 	private String description;
 	private String title;
-	private Employee[] participants;
+	private List<Employee> participants;
 	private Locales locales;
 	
 	public Event() {
-		locales = new Locales();
-		
+		locales = new Locales();	
 	}
 	
 	public Employee[] getEmployeeList() { //Get employees from database
@@ -37,16 +35,81 @@ public class Event {
 		}
 	}
 	
-	public void reserveLocale(datamodell.Locale locale, Time start, Time end) {
+//Reserve location in locales and set locale here
+	public void reserveLocale(Locale locale, Time start, Time end) {
 		locales.setReservedLocale(locale, start, end);
+		setLocale(locale);
 	}
-	
+
+//Reserve name as location (location is not meeting room)
 	public void reserveLocale(String name, Time start, Time end) {
-		locales.setReservedLocale(name, start, end);
+		Locale l = new Locale(name);
+		locales.setReservedLocale(l, start, end);
+		setLocale(l);
 	}
-	
+
+//Returns available locales as boolean table (1 is reserved, 0 is not)
 	public boolean[][] getAvailableLocales(Time start, Time end) {
 		return locales.getLocales(start, end);
 	}
 
+	public int getID() {
+		return ID;
+	}
+	
+	public void setID(int iD) {
+		ID = iD;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+	
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	public Time getTime() {
+		return time;
+	}
+	
+	public void setTime(Time time) {
+		this.time = time;
+	}
+	
+	public Locale getLocale() {
+		return locale;
+	}
+	
+	public void setLocale(Locale locale) {
+		this.locale = locale;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public List<Employee> getParticipants() {
+		return participants;
+	}
+	
+	public void addParticipants(Employee participant) {
+		participants.add(participant);
+	}
+	
+	public Locales getLocales() {
+		return locales;
+	}
 }
