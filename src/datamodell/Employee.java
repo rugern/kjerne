f7619c8e01ee68393f5d0524d547ef7bbe0d92f7
jhnoor <jamawadi@gmail.com>
2013-03-07 13:54:44 +0100

    SocketServer first draft
    Tis' getting a bit drafty, eh guv'nor?

--- src/datamodell/Employee.java
+++ src/datamodell/Employee.java
@@ -3,22 +3,12 @@ package datamodell;
 //Dette er en test fra Vikas
 
 public class Employee extends EventMaker {
-
-	private int ssn;
 	
 	public Employee(String email) {
 		super(email);
 	}
 
+	private int ssn;
 	
-	/**
-	 * This EventMaker has been invited to an event, add to list of invitations 
-	 * and fire event so that user sees this if already logged on.
-	 * @param event
-	 */
-	public void inviteToEvent(Event event) {
-		//TODO PropertyChangeListener maybe?
-		invitations.add(event);
-	}
 	
 }
