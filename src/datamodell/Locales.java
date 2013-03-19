28dc6eb767a3eaf46b1add7cebaeb69f0cfd01e5
bokormen <oyvilund@stud.ntnu.no>
2013-03-19 10:59:31 +0100

    Update Locales.java
    Lastet opp siste versjon igjen

--- src/datamodell/Locales.java
+++ src/datamodell/Locales.java
@@ -1,30 +1,37 @@
 package datamodell;
 
-import GUI.*;
-import java.sql.Time;
-import java.util.Date;
 import java.sql.ResultSet;
-import java.util.ArrayList;
+import java.sql.SQLException;
+import database.Query;
 
-public class Locales {
-//	private static String sql = "SELECT Roomnr, StartTime, EndTime FROM Locale AS L INNER JOIN Event AS E ON (L.Roomnr=E.Roomnr)"; //returns all rooms in use in a event, and the start time and end time for that event
+public class Locales extends Query {
 	
+	/**
+	 * This function gets four strings, a clock time as a string on the form hh:mm and a date time as a string on the form DD/MM/YYYY as a start time and a clock time as a string on the form hh:mm and a date time as a string on the form DD/MM/YYYY and returns an int array with all the available roomnr in the given period
+	 * @param startTime
+	 * @param startDate
+	 * @param endTime
+	 * @param endDate
+	 * @return
+	 * @throws SQLException
+	 */
 	@SuppressWarnings("null")
-	public int[] getLocales(String startTime, String endTime) { // får jeg dette som string???
+	public int[] getLocales(String startTime, String startDate, String endTime, String endDate) throws SQLException {
 		ResultSet result;
-		double[][] Rooms = null;
+		long[][] Rooms = null;
 		int[] availableRooms = null;
-		double start;
-		double end;
+		long start = StringsToLong(startDate,startTime);
+		long end = StringsToLong(endDate, endTime);
 		
-		result = getRoomsAndDate(); //returns the result after an sql query for all rooms(int) in use in a event and the connected start time(String)(tt:mm), end time(String)(tt:mm), start date (String)(dd/mm/YYYY) and end date(String)(dd/mm/YYYY) for these.
+		result = getRoomsAndDate(); //returns the result after an sql query for all rooms(int) in use in a event and the connected start time(String)(hh:mm), end time(String)(hh:mm), start date (String)(DD/MM/YYYY) and end date(String)(DD/MM/YYYY) for these.
 		
 		int i=0;
 		
+//		takes the result from sql query and put it in the table Rooms
 		while (result.next()) {
 			Rooms[i][0]=result.getInt(1);
-			Rooms[i][1]=StringsToDouble(result.getString(3),result.getString(1)); //saves the start time as an double on the form YYYYMMDDhhmm
-			Rooms[i][2]=StringsToDouble(result.getString(4),result.getString(2)); //saves the end time as an double on the form YYYYMMDDhhmm
+			Rooms[i][1]=StringsToLong(result.getString(3),result.getString(1)); //saves the start time as a long on the form YYYYMMDDhhmm
+			Rooms[i][2]=StringsToLong(result.getString(4),result.getString(2)); //saves the end time as a long on the form YYYYMMDDhhmm
 			
 			i++;
 		}
@@ -32,6 +39,7 @@ public class Locales {
 		i=0;
 		int j=0;
 		
+//		Put rooms from the table Rooms that isn't used in the given start and end time and put the roomnr in the table availableRooms
 		while (i < Rooms.length) {
 			if (Rooms[i][1]>end) {
 				availableRooms[j] = (int) Rooms[i][0];
@@ -45,21 +53,22 @@ public class Locales {
 		return availableRooms;
 	}
 	
-	public double StringsToDouble(String date, String time) {
+	
+	/**
+	 * This function takes to strings, one is a date on the form DD/MM/YYYY, and the other one is a clock time on the form hh:mm, and returns it as a long value on the form YYYYMMDDhhmm
+	 * @param date
+	 * @param time
+	 * @return
+	 */
+	public long StringsToLong(String date, String time) {
 		String strDate = date.substring(6, 10);
 		strDate += date.substring(3, 5);
 		strDate += date.substring(0, 2);
 		strDate += time.substring(0, 2);
 		strDate += time.substring(3, 5);
-		double doubleDate = Double.parseDouble(strDate);
+		long longDate = Long.parseLong(strDate);
 		
-		return doubleDate;
+		return longDate;
 	}
 	
-	//Sets locale as reserved from start time to end time
-	public void setReservedLocale(String locale, String start, String end) {
-		//TODO, needed for event class
-		long startDate = Long.parseLong(start);
-		long endDate = Long.parseLong(end);
-	}
 }
